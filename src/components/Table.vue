bb96d2971abce7c106b2745431c850633b09eb2c
zhangningle <1160030724@qq.com>
2020-04-25 22:46:29 +0800

    仿照 iview 的 Table 写的第一个类似的Table组件， 哈哈 挺好的

---
+++ src/components/Table.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <table border='1'>
+            <thead>
+                <td v-for="(item, index) in columns" :key='index'>
+                    {{item.title}}
+                </td>
+            </thead>
+            <tbody>
+                <tr v-for="(item, index) in data" :key="index">
+                    <td v-for="(citem, cindex) in columns" :key='cindex'>
+                        {{item[citem.key]}}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['columns', 'data']
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Table :data="data" :columns="columns"></Table>
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import Table from '@/components/Table.vue'
+
+  export default {
+    name: 'Home',
+    components: {
+      HelloWorld,
+      Table
+    },
+    data() {
+      return {
+
+        columns: [{
+            title: 'Name',
+            key: 'name'
+          },
+          {
+            title: 'Age',
+            key: 'age'
+          },
+          {
+            title: 'Address',
+            key: 'address'
+          },
+          {
+            title: 'Date',
+            key: 'date'
+          }
+        ],
+        data: [{
+            name: 'John Brown',
+            age: 18,
+            address: 'New York No. 1 Lake Park',
+            date: '2016-10-03'
+          },
+          {
+            name: 'Jim Green',
+            age: 24,
+            address: 'London No. 1 Lake Park',
+            date: '2016-10-01'
+          },
+          {
+            name: 'Joe Black',
+            age: 30,
+            address: 'Sydney No. 1 Lake Park',
+            date: '2016-10-02'
+          },
+          {
+            name: 'Jon Snow',
+            age: 26,
+            address: 'Ottawa No. 2 Lake Park',
+            date: '2016-10-04'
+          }
+        ]
+      }
+    }
+  }
+</script>
